fe1113aaa3e28fcc88b764172bcea4a475b2f78b
Grupo Altum <grupoaltum@MacBook-Pro-de-Grupo.local>
2023-01-04 14:17:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# SQL-Server-PHP-Laravel
+Alternativa de Conexión a SQL Server desde Laravel PHP
+
+[![N|Solid](https://i.ibb.co/ZLzQTpm/Firma-Git-Hub.png)](#)
+
+Clase para conexión a servidores SQL Server, con uso de métodos claros y fáciles de conexión.
+Esta clase es una alternativa a la conexión por el ORM Eloquent.
+Clase liviana y precisa para ejecutar cualquier tipo de sentencia en la base de datos.
+
+## Instalación
+
+Descargue el contenido del repositorio a su equipo.
+Copie el contenido del paquete a la ruta _app\Services (Si la carpeta Services no existe, creela.)
+
+```sh
+//Ruta Del Archivo
+app/Services/SQLServer.php;
+```
+
+Para llamado en los controladores, invoque el uso.
+
+```sh
+Use App\Services\SQLServer;
+```
+
+# Cree un Helper que llame los valores de la conexion los cuales deben estar creados en el ENV del proyecto
+El valor "instance" solo debe ir en los casos donde la conexión requiere una instancia especifica, de lo contrario no incluirlo en el helper.
+
+# Variables en el ENV.
+```sh
+DB_SQLSVR_NAME="10.25.21.170"
+DB_SQLSVR_INSTANCE="PROD" //No es Obligatoria, no poner en caso de no tener instancia
+DB_SQLSVR_DATABASE="basededatos"
+DB_SQLSVR_USER="usuario"
+DB_SQLSVR_PASS="contraseña"
+```
+
+# Funcion en los Helpers 
+```sh
+function CREDENCIALES(){
+    $credenciales = array(
+        'server'   => env('DB_SQLSVR_NAME'),
+        'instance' => env('DB_SQLSVR_INSTANCE'), //No es Obligatoria, no poner en caso de no tener instancia
+        'database' => env('DB_SQLSVR_DATABASE'),
+        'user'     => env('DB_SQLSVR_USER'),
+        'password' => env('DB_SQLSVR_PASS')
+    );
+    return $credenciales;
+}
+```
+
+# Genere la conexion a la base de datos de la siguiente forma.
+```sh
+SQLServer::database(CREDENCIALES())
+```
+
+# Comandos con retorno de Datos #
+
+| COMANDO | DESCRIPCIÓN |
+| ----------- | ----------- |
+| SQLServer::database(CREDENCIALES())->select("SELECT * FROM 'usuarios'")->get() | Ejecute las consultas que requiera a la base de datos y genere la respuesta con el metodo ->get(), usando sentencias propias de SQL |
+| SQLServer::database(CREDENCIALES())->procedure("EXEC procedure")->get() | Ejecute los procedimientos almacenados en la base de datos y genere la respuesta con el metodo ->get(), usando sentencias propias de SQL |
+| SQLServer::database(CREDENCIALES())->noCount()->procedure("EXEC procedure")->get() | Ejecute los procedimientos almacenados en la base de datos y genere la respuesta con el metodo ->get(), usando sentencias propias de SQL , El metodo noCount puede ser necesario en consultas a procedimientos que contangan OpenQuerys |
+
+
+# Comandos Adicionales con retorno de Datos #
+
+| COMANDO | DESCRIPCIÓN |
+| ----------- | ----------- |
+| SQLServer::database(CREDENCIALES())->procedure("EXEC procedure")->first() | Trae el primer valor (TOP 1) |
+| SQLServer::database(CREDENCIALES())->procedure("EXEC procedure")->last() | Trae el ultimo valor (TOP 1) |
+| SQLServer::database(CREDENCIALES())->procedure("EXEC procedure")->count() | Cuenta el total de los registros de Respuesta |
+| SQLServer::database(CREDENCIALES())->procedure("EXEC procedure")->reverse() | Retorna los valores de respuesta en Reversa |
+| SQLServer::database(CREDENCIALES())->procedure("EXEC procedure")->collect() | Retorna la respuesta como colección de Laravel |
+
+# Comandos con retorno Booleano true o false #
+
+| COMANDO | DESCRIPCIÓN |
+| ----------- | ----------- |
+| SQLServer::database(CREDENCIALES())->update("UPDATE 'usuarios' SET 'nombre' = 'Raul' WHERE 'id' = 1") | Actualice los datos que requiera en la base de datos, usando sentencias propias de SQL |
+| SQLServer::database(CREDENCIALES())->insert("INSERT INTO 'users' ('id', 'name') VALUES (1, 'Administrador')") | Inserte los datos que requiera en la base de datos, usando sentencias propias de SQL |
+| SQLServer::database(CREDENCIALES())->delete("DELETE FROM 'users' WHERE 'id'=1") | Inserte los datos que requiera en la base de datos, usando sentencias propias de SQL |
+| SQLServer::database(CREDENCIALES())->procedure("EXEC procedure", false) | Ejecute los procedimientos almacenados en la base de datos y genere la respuesta True o False |
+
+
+## Desarrollador
+
+Ingeniero, Raúl Mauricio Uñate Castro
+raulmauriciounate@gmail.com
---
+++ src/SQLServer.php
@@ -0,0 +1,290 @@
+<?php
+
+namespace Rmunate\SqlServerLite;
+
+/**
+ * Clase para conexion con SQL Server
+ * --------------------------------------------
+ * Desarrollado por: Raul Mauricio Uñate Castro
+ * raulmauriciounate@gmail.com
+ */
+
+use PDO;
+use Exception;
+use Throwable;
+use PDOException;
+
+class SQLServer {
+
+#--------------------------╔═════════════════════════════════╗--------------------------#
+#--------------------------║      ATRIBUTOS DEL OBJETO       ║--------------------------#
+#--------------------------╚═════════════════════════════════╝--------------------------#
+
+    protected $status_connection;
+    protected $server_name;
+    protected $user_database;
+    protected $password_database;
+    protected $sqlsrv_attr_query_timeout;
+    protected $PDO;
+    protected $error;
+    private $response;
+    public $nocount = false;
+
+    /* Datos al inicial la clase */
+    private static $server;
+    private static $instance;
+    private static $database;
+    private static $user;
+    private static $password;
+    private static $timeout;
+    private static $use_expecific_instance = true;
+
+#--------------------------╔═════════════════════════════════╗--------------------------#
+#--------------------------║      CONSTRUCTOR DE LA CLASE    ║--------------------------#
+#--------------------------╚═════════════════════════════════╝--------------------------#
+
+    public function __construct(){
+        /* Valores de Conexion a la Base de Datos */
+        if(Self::$use_expecific_instance){
+            $this->server_name = Self::$server . chr(92) . Self::$instance . chr(59) . Self::$database;
+        } else {
+            $this->server_name = Self::$server . chr(59) . Self::$database;
+        }
+        $this->user_database = Self::$user;
+        $this->password_database = Self::$password;
+        $this->sqlsrv_attr_query_timeout = Self::$timeout ?? 0;
+        /* Creacion de la Conexion */
+        try {
+            $conn = new PDO($this->server_name, $this->user_database, $this->password_database);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->setAttribute(PDO::SQLSRV_ATTR_QUERY_TIMEOUT,$this->sqlsrv_attr_query_timeout);
+            $this->PDO = $conn;
+            $this->status_connection = true;
+            $this->error = null;
+        } catch (PDOException $e) {
+            $this->PDO = null;
+            $this->status_connection = false;
+            $this->error = implode($e->errorInfo);
+        }
+    }
+
+#--------------------------╔═════════════════════════════════╗--------------------------#
+#--------------------------║         INICIALIZACION          ║--------------------------#
+#--------------------------╚═════════════════════════════════╝--------------------------#
+
+    public static function database(array $arrayCredentials){
+        /* Validar que todos los datos vengan en la clase */
+        if (!isset($arrayCredentials['server'])) {
+            /* Valor Obligatorio */
+            throw new Exception('No se encuentra el index [server] en el arreglo ingresado en el metodo database');
+        } else if (!isset($arrayCredentials['database'])){
+            throw new Exception('No se encuentra el index [database] en el arreglo ingresado en el metodo database');
+        } else if (!isset($arrayCredentials['user'])){
+            throw new Exception('No se encuentra el index [user] en el arreglo ingresado en el metodo database');
+        } else if (!isset($arrayCredentials['password'])){
+            throw new Exception('No se encuentra el index [password] en el arreglo ingresado en el metodo database');
+        } else {
+            /* Creacion de Datos en Valores Estaticos */
+            Self::$server = 'sqlsrv:Server=' . trim($arrayCredentials['server']);
+            Self::$database = 'Database=' . trim($arrayCredentials['database']);
+            Self::$user = trim($arrayCredentials['user']);
+            Self::$password = trim($arrayCredentials['password']);
+            Self::$timeout = 0; /* Sin Tiempo Definido Para Detener El Script*/
+
+            /* Definir Uso de Instancia Especifica */
+            if(isset($arrayCredentials['instance']) && $arrayCredentials['instance'] != ''){
+                Self::$use_expecific_instance = true;
+                Self::$instance = trim($arrayCredentials['instance']);
+
+            } else {
+                Self::$use_expecific_instance = false;
+                Self::$instance = '';
+            }
+
+            return new static();
+        }
+    }
+
+#--------------------------╔═════════════════════════════════╗--------------------------#
+#--------------------------║            CONSULTAS            ║--------------------------#
+#--------------------------╚═════════════════════════════════╝--------------------------#
+
+    /* ▼ SET NOCOUNT ON ▼ */
+    public function noCount(){
+        $this->nocount = "SET NOCOUNT ON;";
+        return $this;
+    }
+
+    /* ▼ PROCEDURE ▼ */
+    public function procedure(string $statement, $return = true){
+        if ($return) {
+            if ($this->status_connection) {
+                $conn = $this->PDO;
+                $query = strval($statement);
+                /* Evitar Errores Con OpenQuery */
+                if ($this->nocount) {
+                    $query = $this->nocount . $query;
+                }
+                $stmt = $conn->query($query);
+                $rows = [];
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                    array_push($rows, $row);
+                }
+                $this->response = $rows;
+                return $this;
+            } else {
+                throw new Exception('No se logro conectar al servidor: ' . $this->server_name . ', error: ' . $this->error);
+            }
+        } else {
+            if ($this->status_connection) {
+                $conn = $this->PDO;
+                $query = strval($statement);
+                //$conn->beginTransaction();
+                try {
+                    $stmt = $conn->exec($query);
+                    //$conn->commit();
+                    return true;
+                } catch (\Throwable $th) {
+                    //$conn->rollback();
+                    return $th->getMessage();
+                }
+            } else {
+                throw new Exception('No se logro conectar al servidor: ' . $this->server_name . ', error: ' . $this->error);
+            }
+        }
+    }
+
+    /* ▼ SELECT ▼ */
+    public function select(string $statement){
+        if ($this->status_connection) {
+            $conn = $this->PDO;
+            $query = strval($statement);
+            $stmt = $conn->query($query);
+            $rows = [];
+
+            /* Creacion de Arreglo Asociativo */
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                array_push($rows, $row);
+            }
+            $this->response = $rows;
+            return $this;
+        } else {
+            throw new Exception('No se logro conectar al servidor: ' . $this->server_name . ', error: ' . $this->error);
+        }
+    }
+
+    /* ▼ RETORNO DE RESULTADO ▼ */
+
+    /* Retorno de todas las respuestas en array */
+    public function get(){
+        if (count($this->response) >= 1) {
+            return $this->response;
+        } else {
+            return $response = [];
+        }
+    }
+
+    /* Retornar la primer llave */
+    public function first(){
+        if (count($this->response) >= 1) {
+            return $this->response[array_key_first($this->response)];
+        } else {
+            return $response = [];
+        }
+    }
+
+    /* Retornar la ultima llave */
+    public function last(){
+        if (count($this->response) >= 1) {
+            return $this->response[array_key_last($this->response)];
+        } else {
+            return $response = [];
+        }
+    }
+
+    /* Retornar la cuenta */
+    public function count(){
+        return count($this->response);
+    }
+
+    /* Retornar la consulta de forma inversa */
+    public function reverse(){
+        if (count($this->response) >= 1) {
+            return array_reverse($this->response);
+        } else {
+            return $response = [];
+        }
+    }
+
+    /* Retornar en coleccion de Laravel */
+    public function collect(){
+        if (count($this->response) >= 1) {
+            return collect($this->response);
+        } else {
+            return $response = collect([]);
+        }
+    }
+
+#--------------------------╔═════════════════════════════════╗--------------------------#
+#--------------------------║            SENTENCIAS           ║--------------------------#
+#--------------------------╚═════════════════════════════════╝--------------------------#
+
+    /* ▼ UPDATE ▼ */
+    public function update(string $statement){
+        if ($this->status_connection) {
+            $conn = $this->PDO;
+            $query = strval($statement);
+            $conn->beginTransaction();
+            try {
+                $stmt = $conn->exec($query);
+                $conn->commit();
+                return true;
+            } catch (\Throwable $th) {
+                $conn->rollback();
+                return $th->getMessage();
+            }
+        } else {
+            throw new Exception('No se logro conectar al servidor: ' . $this->server_name . ', error: ' . $this->error);
+        }
+    }
+
+    /* ▼ INSERT ▼ */
+    public function insert(string $statement){
+        if ($this->status_connection) {
+            $conn = $this->PDO;
+            $query = strval($statement);
+            $conn->beginTransaction();
+            try {
+                $stmt = $conn->exec($query);
+                $conn->commit();
+                return true;
+            } catch (\Throwable $th) {
+                $conn->rollback();
+                return $th->getMessage();
+            }
+        } else {
+            throw new Exception('No se logro conectar al servidor: ' . $this->server_name . ', error: ' . $this->error);
+        }
+    }
+
+    /* ▼ DELETE ▼ */
+    public function delete(string $statement){
+        if ($this->status_connection) {
+            $conn = $this->PDO;
+            $query = strval($statement);
+            $conn->beginTransaction();
+            try {
+                $stmt = $conn->exec($query);
+                $conn->commit();
+                return true;
+            } catch (\Throwable $th) {
+                $conn->rollback();
+                return $th->getMessage();
+            }
+        } else {
+            throw new Exception('No se logro conectar al servidor: ' . $this->server_name . ', error: ' . $this->error);
+        }
+    }
+}
+
+?>
